dbaf2f5effef07336e9e5e0f9d1663908a5a100f
Mike Cutalo <mcutalo88@gmail.com>
2014-07-14 22:23:58 -0400

    Moar Trasporter

--- SpaceEngineersBackUtility/Transporter.cs
+++ SpaceEngineersBackUtility/Transporter.cs
@@ -4,35 +4,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.IO;
+
 namespace SpaceEngineersBackUtility
 {
     class Transporter
     {
         public String destination;
-        public String origin;
+        public String source;
             
         public Transporter()
         {
             this.destination = "";
-            this.origin = "";
+            this.source = "";
         }
 
 
         /*
-         * Validate File that origin is pointing too.
+         * Validate File that source is pointing too.
          */
-        public void validateFile(){
-
+        public Boolean validateFile(){
+            if (File.Exists(source))
+                return true;
+            else
+                return false;
         }
 
         /*
-         * Moves File to the destination. 
+         * Moves File to the destination.
+         * TODO: make backup a service.
          */
         public void backUp(){
-
+            try{
+                //Validate...
+                File.Copy(source, destination);
+                //Update the DB - ..
+            }
+            catch (FileNotFoundException ex){
+                Console.Write(ex);
+            }
         }
-
-
-
     }
 }
