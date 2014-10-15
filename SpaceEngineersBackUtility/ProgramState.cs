f22d23ad2d79942d49232e1e1a367b675a0801ab
zachmilo <zachmilo45@gmail.com>
2014-10-15 13:47:08 -0400

    Date 10.15.14

---
+++ SpaceEngineersBackUtility/ProgramState.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceEngineersBackUtility
+{
+    class ProgramState
+    {
+        String backupPath;
+        bool localBackup; 
+        bool dedicatedSErverBackup;
+        bool useDefaultPath;
+        bool customSavePath; 
+        String customeSaveInput;  
+        bool button4;
+        bool button3; 
+
+      public ProgramState()
+      {
+          this.backupPath = "";
+          this.localBackup = true;
+          this.dedicatedSErverBackup = true;
+          this.useDefaultPath = true;
+          this.customeSaveInput = "";
+          this.button4 = true;
+          this.button3 = true; 
+   
+      }
+       
+      /* Saves user input from the UI. **/  
+      public void SaveInput(String backupPath, bool localBackup, bool dedicatedServerBackup, bool useDefaultPath, bool customSavePath, String customeSaveInput, bool button4, bool button3)
+      {
+          SpaceEngineerLINQDataContext dc = new SpaceEngineerLINQDataContext();
+          Table<UiSaveState> t = dc.GetTable<UiSaveState>();
+          List <UiSaveState> saveState= new List <UiSaveState>();
+          UiSaveState back = new UiSaveState();
+          int size=saveState.Count(); 
+ 
+          if (size>1)
+          {
+              saveState.Clear(); 
+          }
+          else
+          {
+            
+             saveState.Add(); 
+          }
+
+          
+          
+          dc.LocalBackups.InsertOnSubmit(back);
+          dc.SubmitChanges();
+
+          
+      }
+    }
+    
+   
+
+}
---
+++ SpaceEngineersBackUtility/SpaceEngineerDAO.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Linq; 
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceEngineersBackUtility
+{
+    class SpaceEngineerDAO
+    {
+        DataContext db = new DataContext(@"space_eng_util_db.mdf");        
+         
+        string backupPath;         // textbox holding seleected path 
+        bool localBackup;           // checkbox for localbackup 
+        bool dedicatedServerBackup; // checkbox for dedicatedServerBackup 
+        bool useDefaultPath;        // radiobutton for useDefaultPath 
+        bool customSavePath;        // radiobutton for customeSavePath 
+        bool enable;                // enable button
+        bool disable;               // disable button
+        string numBackups;          // holds number of backups status today 
+        string lastBackup;          // last backup status
+        string nextBackup;          // next scheduled backup  
+        
+        public SpaceEngineerDAO(){
+        }
+
+        public string getBackupPath(){
+            return backupPath; 
+        }
+        public void setBackupPath(string hold){
+            this.backupPath = hold; 
+        }
+
+        public void writeToDBTest(){
+           SpaceEngineerLINQDataContext dc = new SpaceEngineerLINQDataContext();
+           Table<LocalBackup> t = dc.GetTable<LocalBackup>();
+           LocalBackup back = new LocalBackup();
+
+           
+           back.Origin = "Poop";
+           dc.LocalBackups.InsertOnSubmit(back);   
+           dc.SubmitChanges(ConflictMode.ContinueOnConflict);
+            //var holdquery = from p in db.GetTable<LocalBackup>() select p.Origin;
+            
+        }
+
+
+    }
+}
